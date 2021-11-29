7ba6a04c9d333de1a10b50dd735b13f252281fcb
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-29 18:37:20 +0100

    Added new React components

---
+++ src/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="hero-container">
+      <img
+        src="./images/photo-grid.png"
+        alt="people-grid"
+        className="hero-image"
+      />
+
+      <h1 className="hero-title">Online Experiences</h1>
+      <p className="hero-content">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
