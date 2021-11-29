97737aac1fd0bf005d50a930258273c7d68697cf
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-29 18:37:32 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Card from "./Card";
 import "./App.css";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <h1>App component</h1>
+      <Hero />
+      <Card />
     </div>
   );
 }
